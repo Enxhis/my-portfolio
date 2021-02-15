7fe2e9ed5a0d4769143261146b2d0d8702003090
Enxhi Salaj <enxhi_salaj@yahoo.com>
2021-02-14 21:14:50 -0500

    added bio component to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,10 @@
 import React from 'react';
-//import Header from '../components/Header';
-//import Footer from '../components/Footer';
+import Bio from '../components/Bio';
 
 function Home() {
     return (
         <div>
-           
+           <Bio />
         </div>
     )
 }
