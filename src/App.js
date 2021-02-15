0e07f49e2868e03e86abb08421a03bf7c7202554
Enxhi Salaj <enxhi_salaj@yahoo.com>
2021-02-14 19:38:37 -0500

    initial setup of basic components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Router>
+        <Route exact path='/'> <Home /></Route>
+        <Route exact path='/Portfolio'> <Portfolio /></Route>
+      </Router>
+      <Footer />
     </div>
   );
 }
