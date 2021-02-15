f8b9c168cf93e7af89f4a85f5f9cb46712232498
Enxhi Salaj <enxhi_salaj@yahoo.com>
2021-02-14 20:18:15 -0500

    added navlink to app.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Nav from 'react-bootstrap/Nav'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Router>
+        <Nav variant="tabs" defaultActiveKey="/home">
+          <Nav.Item>
+            <Nav.Link href="/Home">Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/Portfolio">Portfolio</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="Contact">Contact</Nav.Link>
+          </Nav.Item>
+        </Nav>
         <Route exact path='/'> <Home /></Route>
         <Route exact path='/Portfolio'> <Portfolio /></Route>
       </Router>
+
       <Footer />
     </div>
   );
