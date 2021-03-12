1022e8e09a23397b87331dc9f520a0832cc61ef6
Enxhi Salaj <enxhi_salaj@yahoo.com>
2021-03-12 13:45:54 -0500

    fixed issues with jquery

--- public/js/init.js
+++ public/js/init.js
@@ -41,23 +41,18 @@ jQuery(document).ready(function ($) {
     var navigation_links = $("#nav-wrap a");
 
     sections.waypoint({
-
         handler: function (event, direction) {
-
             var active_section;
-
             active_section = $(this);
             if (direction === "up") active_section = active_section.prev();
-
             var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
-
             navigation_links.parent().removeClass("current");
             active_link.parent().addClass("current");
-
         },
         offset: '35%'
-
     });
+
+
     /*----------------------------------------------------*/
     /*	Make sure that #header-background-image height is
     /* equal to the browser height.
@@ -65,7 +60,6 @@ jQuery(document).ready(function ($) {
 
     $('header').css({ 'height': $(window).height() });
     $(window).on('resize', function () {
-
         $('header').css({ 'height': $(window).height() });
         $('body').css({ 'width': $(window).width() })
     });
@@ -76,7 +70,6 @@ jQuery(document).ready(function ($) {
     ------------------------------------------------------*/
 
     $(window).on('scroll', function () {
-
         var h = $('header').height();
         var y = $(window).scrollTop();
         var nav = $('#nav-wrap');
@@ -92,7 +85,6 @@ jQuery(document).ready(function ($) {
                 nav.addClass('opaque').fadeIn('fast');
             }
         }
-
     });
 
 
@@ -101,13 +93,11 @@ jQuery(document).ready(function ($) {
     ------------------------------------------------------*/
 
     $('.item-wrap a').magnificPopup({
-
         type: 'inline',
         fixedContentPos: false,
         removalDelay: 200,
         showCloseBtn: false,
         mainClass: 'mfp-fade'
-
     });
 
     $(document).on('click', '.popup-modal-dismiss', function (e) {
@@ -130,4 +120,49 @@ jQuery(document).ready(function ($) {
         animationSpeed: 600,
         randomize: false,
     });
+
+    /*----------------------------------------------------*/
+    /*	contact form
+    ------------------------------------------------------*/
+
+    // $('form#contactForm button.submit').click(function() {
+
+    //    $('#image-loader').fadeIn();
+
+    //    var contactName = $('#contactForm #contactName').val();
+    //    var contactEmail = $('#contactForm #contactEmail').val();
+    //    var contactSubject = $('#contactForm #contactSubject').val();
+    //    var contactMessage = $('#contactForm #contactMessage').val();
+
+    //    var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
+    //             '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
+
+    //    $.ajax({
+
+    //       type: "POST",
+    //       url: "inc/sendEmail.php",
+    //       data: data,
+    //       success: function(msg) {
+
+    //          // Message was sent
+    //          if (msg == 'OK') {
+    //             $('#image-loader').fadeOut();
+    //             $('#message-warning').hide();
+    //             $('#contactForm').fadeOut();
+    //             $('#message-success').fadeIn();   
+    //          }
+    //          // There was an error
+    //          else {
+    //             $('#image-loader').fadeOut();
+    //             $('#message-warning').html(msg);
+    //             $('#message-warning').fadeIn();
+    //          }
+
+    //       }
+
+    //    });
+    //    return false;
+    // });
+
+
 });
